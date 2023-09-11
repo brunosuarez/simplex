941ba04c971b5b5fb63eab7f7d48f35ab6c43dae
bruno.suarez <contatobrunosuarez@gmail.com>
2023-09-11 19:00:50 -0300

    subindo códigos comentados

--- simplexMAX.py
+++ simplexMAX.py
@@ -1,17 +1,16 @@
 class Simplex:
-
     def __init__(self):
-        self.table = []
+        self.table = []  # Inicializa a tabela do algoritmo Simplex.
 
     def set_objective_function(self, fo: list):
-        self.table.append(fo)
+        self.table.append(fo)  # Define a Função Objetivo na primeira linha da tabela.
 
     def add_restrictions(self, sa: list):
-        self.table.append(sa)
+        self.table.append(sa)  # Adiciona as restrições à tabela.
 
     def get_entry_column(self) -> int:
-        column_pivot = min(self.table[0])
-        index = self.table[0].index(column_pivot)
+        column_pivot = min(self.table[0])  # Encontra a coluna do pivô (menor valor na primeira linha).
+        index = self.table[0].index(column_pivot)  # Encontra o índice da coluna do pivô.
         return index
     
     def get_exit_line(self, entry_column: int) -> int:
@@ -20,55 +19,61 @@ class Simplex:
             if line > 0:
                 if self.table[line][entry_column] > 0:
                     division = self.table[line][-1] / self.table[line][entry_column]
-                    results[line] = division
-        index = min(results, key=results.get) 
+                    results[line] = division  # Calcula as razões e as armazena em um dicionário.
+        
+        index = min(results, key=results.get)  # Encontra a linha de saída com a menor razão.
         return index     
     
     def calculate_new_pivot_line(self, entry_column: int, exit_line: int) -> list:
-        line = self.table[exit_line]
-        pivot = line[entry_column]
-        new_pivot_line = [value / pivot for value in line]
+        line = self.table[exit_line]  # Obtém a linha de saída.
+        pivot = line[entry_column]  # Encontra o valor do pivô.
+        new_pivot_line = [value / pivot for value in line]  # Calcula a nova linha do pivô.
         return new_pivot_line
 
     def calculate_new_line(self, line: list, entry_column: int, pivot_line: list) -> list:
-        pivot = line[entry_column] * -1
-        result_line = [value * pivot for value in pivot_line]
-        new_line = [line[i] + result_line[i] for i in range(len(result_line))]
+        pivot = line[entry_column] * -1  # Calcula o negativo do coeficiente do pivô.
+        result_line = [value * pivot for value in pivot_line]  # Calcula a nova linha do resultado.
+        new_line = [line[i] + result_line[i] for i in range(len(result_line))]  # Calcula a nova linha da tabela.
         return new_line
         
     def is_negative(self) -> bool:
-        negative = list(filter(lambda x: x < 0, self.table[0]))
+        negative = list(filter(lambda x: x < 0, self.table[0]))  # Verifica se há coeficientes negativos na Função Objetivo.
         return True if len(negative) > 0 else False
         
     def show_table(self):
         for i in range(len(self.table)):
             for j in range(len(self.table[0])):
-                print(f"*{self.table[i][j]}\t", end="")
+                print(f"*{self.table[i][j]}\t", end="")  # Exibe a tabela formatada.
             print()    
 
     def calculate(self):
         entry_column = self.get_entry_column()
+
         first_exit_line = self.get_exit_line(entry_column)
+
         pivot_line = self.calculate_new_pivot_line(entry_column, first_exit_line)
-        self.table[first_exit_line] = pivot_line
+
+        self.table[first_exit_line] = pivot_line  # Atualiza a linha de saída pela nova linha do pivô.
+
         table_copy = self.table.copy()
+
         index = 0
         while index < len(self.table):
             if index != first_exit_line:
                 line = table_copy[index]
                 new_line = self.calculate_new_line(line, entry_column, pivot_line)
-                self.table[index] = new_line
+                self.table[index] = new_line  # Calcula e atualiza as novas linhas da tabela.
             index += 1
 
 def solve_simplex():
     simplex = Simplex()
-    simplex.set_objective_function([1, -5, -2, 0, 0, 0])
-    simplex.add_restrictions([0, 2, 1, 1, 0, 6])
-    simplex.add_restrictions([0, 10, 12, 0, 0, 1, 60])
-    simplex.calculate()
-    while simplex.is_negative():
-        simplex.calculate()
-    simplex.show_table()
+    simplex.set_objective_function([1, -5, -2, 0, 0, 0])  # Define a Função Objetivo.
+    simplex.add_restrictions([0, 2, 1, 1, 0, 6])  # Adiciona a primeira restrição.
+    simplex.add_restrictions([0, 10, 12, 0, 0, 1, 60])  # Adiciona a segunda restrição.
+    simplex.calculate()  # Inicia o processo Simplex.
+    while simplex.is_negative():  # Continua enquanto houver coeficientes negativos na Função Objetivo.
+        simplex.calculate()  # Calcula uma nova iteração.
+    simplex.show_table()  # Exibe a tabela final.
 
 if __name__ == "__main__":
-    solve_simplex()
+    solve_simplex()  # Chama a função principal para resolver o problema de maximização.
--- simplexMIN.py
+++ simplexMIN.py
@@ -1,15 +1,16 @@
 class SimplexMinimization:
 
     def __init__(self):
-        self.table = []
+        self.table = []  # Cria uma tabela vazia para representar o tableau do Simplex.
 
     def set_objective_function(self, fo: list):
-        self.table.append([-val for val in fo])
+        self.table.append([-val for val in fo])  # Configura a função objetivo (FO) no tableau, negando os coeficientes para minimização.
 
     def add_restrictions(self, sa: list):
-        self.table.append(sa)
+        self.table.append(sa)  # Adiciona as restrições (subject to, SA) ao tableau.
 
     def get_entry_column(self) -> int:
+        # Encontra a coluna de entrada, que é a coluna com o menor valor na linha de FO (Função Objetivo).
         column_pivot = min(self.table[0])
         index = self.table[0].index(column_pivot)
         return index
@@ -18,76 +19,92 @@ class SimplexMinimization:
         results = {}
         for line in range(len(self.table)):
             if line > 0:
-                if self.table[line][entry_column] < 0:  # Alteração aqui para tratar negativos
-                    division = -self.table[line][-1] / self.table[line][entry_column]  # Alteração aqui
+                if self.table[line][entry_column] < 0:  # Encontra as linhas onde o valor na coluna de entrada é negativo.
+                    division = -self.table[line][-1] / self.table[line][entry_column]  # Calcula a razão.
                     results[line] = division
 
         if not results:
-            raise Exception("O problema é ilimitado.")
+            raise Exception("O problema é ilimitado.")  # Se não houver razões positivas, o problema é ilimitado.
 
-        index = min(results, key=results.get)
+        index = min(results, key=results.get)  # Encontra a linha de saída com a menor razão positiva.
         return index
 
     def calculate_new_pivot_line(self, entry_column: int, exit_line: int) -> list:
         line = self.table[exit_line]
         pivot = line[entry_column]
+
+        # Calcula a nova linha de pivô dividindo todos os elementos na linha de saída pelo valor do pivô.
         new_pivot_line = [value / pivot for value in line]
         return new_pivot_line
 
     def calculate_new_line(self, line: list, entry_column: int, pivot_line: list) -> list:
         pivot = line[entry_column] * -1
+
+        # Calcula a nova linha multiplicando a linha de pivô pela entrada atual e somando-a à linha atual.
         result_line = [value * pivot for value in pivot_line]
         new_line = [line[i] + result_line[i] for i in range(len(result_line))]
         return new_line
 
     def is_negative(self) -> bool:
         negative = list(filter(lambda x: x < 0, self.table[0]))
+
+        # Verifica se algum coeficiente na linha de FO é negativo, indicando que o algoritmo ainda não terminou.
         return True if len(negative) > 0 else False
 
     def show_table(self):
+        # Exibe a tabela completa, útil para depuração.
         for i in range(len(self.table)):
             for j in range(len(self.table[0])):
                 print(f"*{self.table[i][j]}\t", end="")
             print()
 
-    def calculate(self):
-
+    def calculate(self): # Encontra a coluna de entrada (variável que entra na base).
         entry_column = self.get_entry_column()
 
-        first_exit_line = self.get_exit_line(entry_column)
+    first_exit_line = self.get_exit_line(entry_column) # Encontra a linha de saída (linha que sai da base).
+    
+    pivot_line = self.calculate_new_pivot_line(entry_column, first_exit_line) # Calcula a nova linha do pivô (linha de saída dividida pelo pivô).
+    
+    self.table[first_exit_line] = pivot_line # Atualiza a tabela substituindo a linha de saída pela nova linha do pivô.
 
-        pivot_line = self.calculate_new_pivot_line(entry_column, first_exit_line)
+    table_copy = self.table.copy() # Cria uma cópia da tabela atual para calcular as novas linhas sem alterar os valores antigos.
 
-        self.table[first_exit_line] = pivot_line
+    index = 0
+    
+    while index < len(self.table): # Calcula as novas linhas da tabela.
+        if index != first_exit_line:
+            line = table_copy[index]
+            new_line = self.calculate_new_line(line, entry_column, pivot_line)
+            self.table[index] = new_line
+        index += 1
 
-        table_copy = self.table.copy()
-
-        index = 0
-
-        while index < len(self.table):
-            if index != first_exit_line:
-                line = table_copy[index]
-                new_line = self.calculate_new_line(line, entry_column, pivot_line)
-                self.table[index] = new_line
-            index += 1
-
-    def solve(self):
+    def solve(self): # Inicia o processo Simplex calculando a primeira iteração.
         self.calculate()
 
-        while self.is_negative():
-            self.calculate()
+    # Continua o processo enquanto houver coeficientes negativos na Função Objetivo.
+    while self.is_negative():
+        self.calculate()  # Calcula uma nova iteração.
 
-        self.show_table()
+    # Quando o processo está completo, exibe a tabela resultante.
+    self.show_table()
 
-def solve_simplex_minimization():
-    simplex = SimplexMinimization()
+    def solve_simplex_minimization(): # Cria uma instância do algoritmo SimplexMinimization.
+        simplex = SimplexMinimization()
+    
+    # Define a função objetivo a ser minimizada e adiciona restrições.
     simplex.set_objective_function([1, 2, 3, 0, 0, 0])
     simplex.add_restrictions([-1, -1, -1, 0, 0, -3])
     simplex.add_restrictions([-1, -2, 0, 0, -2, -2])
+    
+    # Inicia o processo de minimização do Simplex.
     simplex.calculate()
-    while simplex.is_negative():
-        simplex.calculate()
-    simplex.show_table()
+
+    while simplex.is_negative(): # Continua o processo enquanto houver coeficientes negativos na Função Objetivo.
+        simplex.calculate()  # Calcula uma nova iteração.
+
+    
+    simplex.show_table() # Quando o processo de minimização está completo, exibe a tabela resultante.
 
 if __name__ == "__main__":
-    solve_simplex_minimization()
+    solve_simplex_minimization()  # Chama a função principal para resolver o problema de minimização.
+
